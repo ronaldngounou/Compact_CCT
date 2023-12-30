73dcbd90474e16603e64eb8b6f6b0e6f0fea10c5
ronaldngounou <landryzinkoue@yahoo.fr>
2023-12-29 20:06:08 -0500

    modified 1d input and incorporated dataset to the CCT

--- utils/tokenizer.py
+++ utils/tokenizer.py
@@ -6,35 +6,30 @@ import torch.nn.functional as F
 class Tokenizer(nn.Module):
     def __init__(self,
                  kernel_size, stride, padding,
-                 pooling_kernel_size=3, pooling_stride=2, pooling_padding=1,
+                 pooling_kernel_size=3,
                  n_conv_layers=1,
-                 n_input_channels=3,
-                 n_output_channels=64,
+                 n_input_channels=2,
+                 n_output_channels=1,
                  in_planes=64,
                  activation=None,
                  max_pool=True,
                  conv_bias=False):
         super(Tokenizer, self).__init__()
 
-        n_filter_list = [n_input_channels] + \
-                        [in_planes for _ in range(n_conv_layers - 1)] + \
-                        [n_output_channels]
-
         self.conv_layers = nn.Sequential(
             *[nn.Sequential(
-                nn.Conv1d(n_filter_list[i], n_filter_list[i + 1],
-                          kernel_size=(kernel_size, kernel_size),
-                          stride=(stride, stride),
-                          padding=(padding, padding), bias=conv_bias),
-                nn.Identity() if activation is None else activation(),
-                nn.MaxPool1d(kernel_size=pooling_kernel_size,
-                             stride=pooling_stride,
-                             padding=pooling_padding) if max_pool else nn.Identity()
+                nn.Conv1d(in_channels=n_input_channels, 
+                          out_channels= n_output_channels,
+                          kernel_size=kernel_size,
+                          stride=stride,
+                          padding= padding, bias=conv_bias),
+                nn.Identity() if activation is None else activation()
+                #nn.MaxPool1d(kernel_size=pooling_kernel_size) if max_pool else nn.Identity()
             )
                 for i in range(n_conv_layers)
             ])
 
-        self.flattener = nn.Flatten(1, -1) 
+        self.activation = activation()
         self.apply(self.init_weight)
 
     def sequence_length(self, n_channels=1, length = 1000):
@@ -42,7 +37,7 @@ class Tokenizer(nn.Module):
 
     def forward(self, x):
         x = self.conv_layers(x)
-        x = self.flatterner(x)
+        x = self.activation(x)
         return x
 
     @staticmethod
@@ -56,9 +51,9 @@ class Tokenizer(nn.Module):
 class TextTokenizer(nn.Module):
     def __init__(self,
                  kernel_size, stride, padding,
-                 pooling_kernel_size=3, pooling_stride=2, pooling_padding=1,
+                 pooling_kernel_size=3, pooling_stride=1, pooling_padding=0,
                  embedding_dim=300,
-                 n_output_channels=128,
+                 n_output_channels=1,
                  activation=None,
                  max_pool=True,
                  *args, **kwargs):
@@ -68,13 +63,12 @@ class TextTokenizer(nn.Module):
         self.conv_layers = nn.Sequential(
             nn.Conv1d(1, n_output_channels,
                       kernel_size=(kernel_size, embedding_dim),
-                      stride=(stride, 1),
-                      padding=(padding, 0), bias=False),
+                      stride=stride,
+                      padding= padding, bias=False),
             nn.Identity() if activation is None else activation(),
             nn.MaxPool1d(
-                kernel_size=pooling_kernel_size,
-                stride=pooling_stride,
-                padding=pooling_padding,
+                kernel_size=pooling_kernel_size
+
             ) if max_pool else nn.Identity()
         )
 
@@ -91,11 +85,11 @@ class TextTokenizer(nn.Module):
             dtype=torch.float)
         new_mask = F.conv1d(
             new_mask, cnn_weight, None,
-            self.conv_layers[0].stride[0], self.conv_layers[0].padding[0], dilation=1, groups=1)
+            self.conv_layers[0].stride[0], self.conv_layers[0].padding[0], groups=1)
         if self.max_pool:
             new_mask = F.max_pool1d(
                 new_mask, self.conv_layers[2].kernel_size[0],
-                stide = self.conv_layers[2].stride[0], padding = self.conv_layers[2].padding[0], dilation=1, ceil_mode=False)
+                stride = self.conv_layers[2].stride[0])
         new_mask = new_mask.squeeze(1)
         new_mask = (new_mask > 0)
         return new_mask
